43df4ed59c1a3abdc7d2335d47fa4baeae7f47ae
unknown <yuweibang@gmail.com>
2013-04-25 16:24:21 +0800

    增加功能

--- blogsender.py
+++ blogsender.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from tkinter import ttk
-import xmlrpc.client
+from xmlrpc import client
 from tkinter import messagebox
+import yaml
+
 #author wbyu123
 root = Tk()
 root.title("BLOG 日志发送器")
@@ -20,6 +22,16 @@ englishname=StringVar()   #英文名
 content=StringVar()   #内容
 
 
+
+f = open('config.yaml')
+# use safe_load instead load
+dataMap = yaml.safe_load(f)
+
+url.set(dataMap['url'])
+method.set(dataMap['method'])
+
+
+
 ttk.Label(mainframe, text="服务器地址:").grid(column=0, row=1, sticky=E)
 feet_entry = ttk.Entry(mainframe, width=17, textvariable=url)
 feet_entry.grid(column=1, row=1, sticky=(W, E))
@@ -29,34 +41,46 @@ feet_entry = ttk.Entry(mainframe, width=12, textvariable=method)
 feet_entry.grid(column=3, row=1, sticky=(W, E))
 
 ttk.Label(mainframe, text="类别:").grid(column=0, row=2, sticky=E)
-feet_entry = ttk.Entry(mainframe, width=5, textvariable=category)
-feet_entry.grid(column=1, row=2, sticky=(W, E))
+w = ttk.Combobox(mainframe, textvariable=category, values=list(dataMap['categorys'].keys()))
+w.grid(column=1, row=2, sticky=(W, E))
 
 ttk.Label(mainframe, text="别名:").grid(column=2, row=2, sticky=E)
 feet_entry = ttk.Entry(mainframe, width=5, textvariable=englishname)
 feet_entry.grid(column=3, row=2, sticky=(W, E))
 
 
-ttk.Label(mainframe, text="标题:").grid(column=0, row=3, sticky=E)
-feet_entry = ttk.Entry(mainframe, width=17, textvariable=title)
-feet_entry.grid(column=1, columnspan=3,row=3, sticky=(W, E))
+codenum=StringVar()   #代码
+ttk.Label(mainframe, text="插入代码:").grid(column=0, row=3, sticky=E)
+codeinsert = ttk.Combobox(mainframe, textvariable=codenum)
+codeinsert['values'] = ('css', 'javascript', 'python','php')
+codeinsert.state(['readonly'])
 
-ttk.Label(mainframe, text="内容:").grid( column=0,row=4, sticky=E)
+codeinsert.grid(column=1, row=3, sticky=(W, E))
+
+ttk.Label(mainframe, text="标题:").grid(column=0, row=4, sticky=E)
+feet_entry = ttk.Entry(mainframe, width=17, textvariable=title)
+feet_entry.grid(column=1, columnspan=3,row=4, sticky=(W, E))
+
+ttk.Label(mainframe, text="内容:").grid( column=0,row=5, sticky=E)
 
 t = Text(mainframe, width=70, height=40)
-t.grid(column=1,columnspan=3,row=4, sticky=(W, E))
+t.grid(column=1,columnspan=3,row=5, sticky=(W, E))
 
 def send():
 	try:
-		proxy = xmlrpc.client.ServerProxy(url.get())
-		script="proxy.{0}(title.get(),t.get(1.0,END),category.get(),englishname.get())".format(method.get());
+		proxy = client.ServerProxy(url.get())
+		categoryid=dataMap['categorys'][category.get()]
+		script="proxy.{0}(title.get(),t.get(1.0,END),categoryid,englishname.get())".format(method.get());
 		result = exec(script)
 		messagebox.showinfo(message='发送成功')
 	except Exception as inst:
 		strg="发送失败:"+ str(inst)
 		messagebox.showinfo(message=strg)
-  
-  
-ttk.Button(mainframe, text="发送", command=send).grid(column=2, row=5, sticky=(W, E))
+
+def insercode(*args):
+	t.insert(END,'<pre class="{0}" name="code">\n</pre>'.format(codenum.get()))
+	
+codeinsert.bind('<<ComboboxSelected>>', insercode)
+ttk.Button(mainframe, text="发送", command=send).grid(column=2, row=6, sticky=(W, E))
 
 root.mainloop()
